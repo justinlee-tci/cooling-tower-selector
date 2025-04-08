b54113fc0b45ad852875c6d3451f7ae03f65de34
justinlee-tci <justinlee@terracooling.com>
2025-04-08 13:46:10 +0800

    icon change

--- app/layout.js
+++ app/layout.js
@@ -17,12 +17,7 @@ export const metadata = {
   title: "NSK Cooling Tower Selector", // Change this to your preferred website name
   description: "Cooling Tower Selection Software for Thermal-Cell NSK Series Cooling Tower", // Update this description
   icons: {
-    icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: { url: '/apple-touch-icon.png' },
+    icon: '/company-icon.ico', // Simplify to just use a single .ico file
   },
 };
 
